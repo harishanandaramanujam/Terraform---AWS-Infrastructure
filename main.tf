2e1607f8937d35f81438db4853c5528f96510227
Harish Ananda Ramanujam <anandaramanujam.h@husky.neu.edu>
2020-06-16 22:48:22 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -236,7 +236,7 @@ resource "aws_route_table_association" "us-east-1a-public-1" {
 
 resource "aws_subnet" "us-east-1a-public-2" {
     vpc_id = "${aws_vpc.northeastern.id}"
-    cidr_block = "192.168.2.0/24"
+    cidr_block = "${var.public_subnet_two}"
     availability_zone = "us-east-1a"
     tags = {
         Name = "Public Subnet 2"
